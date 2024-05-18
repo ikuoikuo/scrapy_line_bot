e966e97ae6c37936ca6f245049b2606541ea2ca2
ikuoikuo <tsukutakun@icloud.com>
2024-05-18 19:19:09 +0900

    LINE友達追加された時の処理の追加

--- libs/line_bot.py
+++ libs/line_bot.py
@@ -8,6 +8,13 @@ def load_user_ids(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file]
 
+def save_user_id(file_path, user_id):
+    """ ファイルにユーザーIDを書き込む """
+    user_ids = load_user_ids(file_path)
+    if user_id not in user_ids:
+        with open(file_path, 'a') as file:
+            file.write(f"{user_id}\n")
+            
 def send_push_message(token, user_id, message_text):
     url = 'https://api.line.me/v2/bot/message/push'
     headers = {
@@ -23,6 +30,19 @@ def send_push_message(token, user_id, message_text):
     }
     response = requests.post(url, headers=headers, json=data)
     return response.status_code, response.text
+
+# 友達追加イベント
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    body = request.get_json()
+    events = body['events']
+
+    for event in events:
+        if event['type'] == 'follow':  
+            user_id = event['source']['userId']
+            save_user_id('../aidb_scrapy/users.csv', user_id)
+
+    return jsonify({"status": "ok"})
 
 @app.route("/send_message")
 def send_message():
